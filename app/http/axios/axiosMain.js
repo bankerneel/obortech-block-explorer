dc5ee18b9d6f25a4e6892414987ba97c21a297b2
Chirag Katrodiya <chirag.katrodiya@solulab.co>
2023-02-06 13:13:22 +0530

    git add .

--- app/http/axios/axiosMain.js
+++ app/http/axios/axiosMain.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const token = `bearer ${localStorage.getItem('userToken')}`;
 
 const axiosMain = axios.create({
-  baseURL: 'http://localhost:3000/api/',
+  baseURL: 'http://winter-cloud-6324.on.fleek.co/api/',
   headers: {
     'Content-Type': 'application/json',
     Authorization: token,
